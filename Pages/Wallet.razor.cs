985d2d460e4fb1079921f18a6e229227aa471a3a
Godspower Otiete <otietegodspower@gmail.com>
2021-10-05 19:14:28 +0100

    fixed up the UI

--- Pages/Wallet.razor.cs
+++ Pages/Wallet.razor.cs
@@ -14,55 +14,50 @@ namespace walletConnect
     {
         public string qrCodeImage = "images/qrcode.jpg";
         public string instructions = "Click button to generate connection code";
-        public bool awaitingConnection = false;
+        public string state = "idle";
 
-        protected override void OnInitialized()
-        {
-            base.OnInitialized();
-            wcService.wc.OnDisconnect += OnWalletSessionDisconnect;
-            wcService.wc.Transport.MessageReceived += OnMessageReceived;
-        }
 
         [Inject]
         public WalletConnectService wcService { get; set; }
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
 
         private Task<WCSessionData> sessionTask;
+
+        protected override void OnInitialized()
+        {
+            wcService.wc.OnDisconnect += OnWalletSessionDisconnect;    
+        }
+
         public async Task ConnectAsync()
         {
             instructions = "Scan QR code with your wallet to complete connection";
-            awaitingConnection = true;
+            state = "awaiting";
             qrCodeImage = $"https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl={HttpUtility.UrlEncode(wcService.wc.URI)}";
             Console.WriteLine($"QR Code :: {wcService.wc.URI}");
-            var data = await wcService.wc.Connect();
-            await wcService.wc.Transport.SendMessage(new NetworkMessage{
-
-            });
-            await wcService.wc.SendRequest<JsonRpcRequest>(new JsonRpcRequest(){
-            },"new Message topic",false);
-        //    client.SendRequestAsync(
-            // await wcService.wc
-            // wcService.wc.Transport.Open(wcService.wc)
+            await wcService.wc.Connect();
+            state = "connected";
+            qrCodeImage = "images/check.png";
         }
 
         public async Task DisconnectAsync()
         {
-            qrCodeImage = "images/qrcode.jpg";
-            instructions = "Click button to generate connection code";
-            awaitingConnection = false;
             await wcService.wc.Disconnect();
+            navigationManager.NavigateTo("/wallet",true);
         }
-
+        public async Task Cancel()
+        {
+           navigationManager.NavigateTo("/wallet",true);
+            state = "idle";
+        }
         private void OnWalletSessionConnect(object sender, WalletConnect wc)
         {
-            Console.WriteLine("Connected");
+            state = "connected";
         }
         private void OnWalletSessionDisconnect(object sender, WalletConnect wc)
         {
-            Console.WriteLine("Disconnected");
+            navigationManager.NavigateTo("/wallet",true);
         }
-        protected void OnMessageReceived(object sender, MessageReceivedEventArgs args)
-        {
-            Console.WriteLine($"{args.Source} : {args.Message.Type} : {args.Message.Topic} : {args.Message.Payload}");
-        }
+        
     }
 }
--- obj/Debug/netstandard2.1/RazorDeclaration/Pages/Wallet.razor.g.cs
+++ obj/Debug/netstandard2.1/RazorDeclaration/Pages/Wallet.razor.g.cs
@@ -13,63 +13,63 @@ namespace walletConnect.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
+#line 1 "c:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
+#line 2 "c:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
+#line 3 "c:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
+#line 4 "c:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
+#line 5 "c:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
+#line 6 "c:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
+#line 7 "c:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
+#line 8 "c:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
 using walletConnect;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
+#line 9 "c:\Users\Expl0it90\Desktop\walletConnect\_Imports.razor"
 using walletConnect.Shared;
 
 #line default
